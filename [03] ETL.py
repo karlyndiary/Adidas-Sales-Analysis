f7c3e97b598d4440b7aedac77d2e3e0f5e5abe33
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-09-01 10:53:30 +0530

    Update [03] ETL.py

--- [03] ETL.py
+++ [03] ETL.py
@@ -1,1 +1,31 @@
+import pandas as pd
+import pyodbc
+from sqlalchemy import create_engine
+import urllib
 
+df = pd.read_csv(r'C:\Users\Karen Fernandes\anaconda3\Files\Projects\Adidas Sales Analysis\adidas_sales_dataset.csv')
+
+# rename Unnamed: 0 column to id
+df.rename(columns={'Unnamed: 0': 'id'}, inplace=True)
+
+server = 'LAPTOP-68P3K4HH\SQLEXPRESS'
+database = 'Adidas'
+username = 'your_username'
+password = 'your_password'
+
+# Create a connection string
+connection_string = f"mssql+pyodbc://{username}:{urllib.parse.quote_plus(password)}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server"
+engine = create_engine(connection_string)
+
+try:
+    with engine.connect() as connection:
+        print("Connection successful!")
+except Exception as e:
+    print(f"Connection failed: {e}")
+
+table_name = 'sales'
+try:
+    df.to_sql(table_name, engine, if_exists='append', index=False)
+    print("Data loaded successfully!")
+except Exception as e:
+    print(f"Data load failed: {e}")
