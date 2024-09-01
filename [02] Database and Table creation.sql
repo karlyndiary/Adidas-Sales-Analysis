48e90ccfd2b1d9fae2dca26725103419114908aa
Karen Judelyn Fernandes <116041695+karlyndiary@users.noreply.github.com>
2024-09-01 10:49:40 +0530

    Create [02] Database and Table creation.sql

---
+++ [02] Database and Table creation.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE Adidas;
+
+USE Adidas;
+
+CREATE TABLE sales (
+	id				  Int PRIMARY KEY,
+    retailer          Varchar(20),        
+    retailer_id       Int,         
+    invoice_date      Date,
+    region            Varchar(50),        
+    state             Varchar(50),        
+    city              Varchar(50),  
+    product           Varchar(100),        
+    price_per_unit    Float,       
+    units_sold        Int,         
+    total_sales       Float,       
+    operating_profit  Float,       
+    operating_margin  Float,       
+    sales_method      Varchar(20),
+	year			  Int,
+	month			  Int,
+	day				  Int
+);
